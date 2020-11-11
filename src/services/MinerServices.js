6623fa63ca0245d4bf87d29e289d65023616ddd6
Andrea Burattin <andrea.burattin@gmail.com>
2020-11-11 17:23:07 +0100

    Fixed protocol and removed localhost instance

--- src/services/MinerServices.js
+++ src/services/MinerServices.js
@@ -1,33 +1,37 @@
 export default class MinerServices {
+    constructor() {
+        this.protocol = "https";
+    }
+
     getMiners(host) {
-        return "http://" + host + "/api/v1/miners";
+        return this.protocol + "://" + host + "/api/v1/miners";
     }
 
     getInstances(host) {
-        return "http://" + host + "/api/v1/instances";
+        return this.protocol + "://" + host + "/api/v1/instances";
     }
 
     createInstance(host, minerId) {
-        return "http://" + host + "/api/v1/instances/" + minerId;
+        return this.protocol + "://" + host + "/api/v1/instances/" + minerId;
     }
 
     getInstanceRunning(host, instanceId) {
-        return "http://" + host + "/api/v1/instances/" + instanceId + "/status";
+        return this.protocol + "://" + host + "/api/v1/instances/" + instanceId + "/status";
     }
 
     getInstanceView(host, instanceId) {
-        return "http://" + host + "/api/v1/instances/" + instanceId + "/view";
+        return this.protocol + "://" + host + "/api/v1/instances/" + instanceId + "/view";
     }
 
     startInstance(host, instanceId) {
-        return "http://" + host + "/api/v1/instances/" + instanceId + "/start";
+        return this.protocol + "://" + host + "/api/v1/instances/" + instanceId + "/start";
     }
 
     stopInstance(host, instanceId) {
-        return "http://" + host + "/api/v1/instances/" + instanceId + "/stop";
+        return this.protocol + "://" + host + "/api/v1/instances/" + instanceId + "/stop";
     }
 
     deleteInstance(host, instanceId) {
-        return "http://" + host + "/api/v1/instances/" + instanceId + "/delete";
+        return this.protocol + "://" + host + "/api/v1/instances/" + instanceId + "/delete";
     }
 }
