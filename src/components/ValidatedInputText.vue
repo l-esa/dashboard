7931399345763a7aef7f111af1e9ffe98f86ea36
Andrea Burattin <andrea.burattin@gmail.com>
2020-11-16 10:51:33 +0100

    Added component for validated input

---
+++ src/components/ValidatedInputText.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-form-group
+            v-bind:label="label"
+            v-bind:state="state">
+            <b-form-input
+                type="text"
+                :placeholder="placeholder"
+                :value="value"
+                @input="update"
+                v-bind:state="state"
+                 />
+        </b-form-group>
+        <b-form-invalid-feedback v-bind:state="state" v-html="error" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ValidatedInputText',
+    props: ['label','placeholder','error','value','state'],
+    methods: {
+        update(newValue) {
+            this.$emit('input', newValue); 
+        }
+    }
+}
+</script>
--- src/modals/NewMiner.vue
+++ src/modals/NewMiner.vue
@@ -4,27 +4,26 @@
         @ok="handleOk"
         title="New Miner">
         <form ref="form" @submit.stop.prevent="handleSubmit">
-            <b-form-group label="Miner host:">
-                <b-form-input
-                    type="text"
-                    v-model="minerHost"
-                    :state="validateState('minerHost')"
-                    required
-                    placeholder="http://miner.host.net"></b-form-input>
-            </b-form-group>
-            <b-form-invalid-feedback :state="validateState('minerHost')">
-                Enter the host of the miner (examples: <code>http://miner.host.net</code> or <code>https://miner.host.net</code>).
-            </b-form-invalid-feedback>
+            <ValidatedInputText
+                label="Miner host:"
+                placeholder="http://miner.host.net"
+                error="Enter the host of the miner (examples: <code>http://miner.host.net</code> or <code>https://miner.host.net</code>)."
+                v-model="minerHost"
+                :state="validateState('minerHost')" />
         </form>
     </b-modal>
 </template>
 
 <script>
-import { required, helpers } from 'vuelidate/lib/validators'
-const host = helpers.regex('host', /^https?:\/\/((?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]|localhost)(:\d{0,5})?$/i)
+import ValidatedInputText from '../components/ValidatedInputText';
+import { required, helpers } from 'vuelidate/lib/validators';
+const host = helpers.regex('host', /^https?:\/\/((?:[a-z0-9](?:[a-z0-9]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9]{0,61}[a-z0-9]|localhost)(:\d{0,5})?$/i);
 
 export default {
     props: ['miners'],
+    components: {
+        ValidatedInputText
+    },
     data() {
         return {
             minerHost: ''
@@ -52,7 +51,7 @@ export default {
         handleOk(bvModalEvt) {
             bvModalEvt.preventDefault();
             this.handleSubmit();
-         },
+         }
     }
 }
 </script>
