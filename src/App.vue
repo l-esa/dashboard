c2236d9766cd0856659a75125654f32d5cfbe701
Andrea Burattin <delas@users.noreply.github.com>
2021-01-19 15:12:30 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -195,8 +195,8 @@ export default {
     this.addStream({processName: "BPIC15_1.xes", brokerHost: "broker.hivemq.com", topicBase: "pmcep"})
     this.addStream({processName: "test", brokerHost: "broker.hivemq.com", topicBase: "pmcep"})
     // this.addMiner({host: "http://localhost:8080"})
-    this.addMiner({host: "http://basic-miners.beamline.cloud"})
-    this.addMiner({host: "http://declare-miners.beamline.cloud"})
+    this.addMiner({host: "https://basic-miners.beamline.cloud"})
+    this.addMiner({host: "https://declare-miners.beamline.cloud"})
   },
   created() {
     this.pollData();
